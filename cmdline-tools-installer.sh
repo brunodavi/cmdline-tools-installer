8b3559d4771bffc7b936904283ec0a06d96561c3
Bruno Davi <Brunodaviandrade2000@gmail.com>
2021-11-01 21:12:13 -0300

    Update cmdline-tools-installer.sh

--- cmdline-tools-installer.sh
+++ cmdline-tools-installer.sh
@@ -1,4 +1,5 @@
-# apt install openjdk-11-jdk
+# Dependencies
+apt install openjdk-11-jdk
 
 dir="sdk/cmdline-tools/latest"
 site="https://developer.android.com/studio"
@@ -7,6 +8,7 @@ repo="https://dl.google.com/android/repository"
 pattern="commandlinetools-linux-\d+_latest.zip"
 name="$(curl -s $site | grep -Pom 1 $pattern)"
 
+# Install command-line-tools
 mkdir -p $dir
 wget -q --show-progress "$repo/$name" 
 unzip -jo $name cmdline-tools -d $dir
@@ -15,6 +17,8 @@ echo "
 export ANDROID_SDK_ROOT='$(realpath ./sdk)'
 " >> ~/.bashrc
 
+# Accept all licenses
 ./$dir/bin/sdkmanager --licences
+
 source ~/.bashrc
 
