00c728020000a6bd7da2239b3a95998fed9c9377
Bruno Davi <Brunodaviandrade2000@gmail.com>
2021-11-01 21:07:42 -0300

    Add files via upload

---
+++ cmdline-tools-installer.sh
@@ -0,0 +1,20 @@
+# apt install openjdk-11-jdk
+
+dir="sdk/cmdline-tools/latest"
+site="https://developer.android.com/studio"
+repo="https://dl.google.com/android/repository"
+
+pattern="commandlinetools-linux-\d+_latest.zip"
+name="$(curl -s $site | grep -Pom 1 $pattern)"
+
+mkdir -p $dir
+wget -q --show-progress "$repo/$name" 
+unzip -jo $name cmdline-tools -d $dir
+
+echo "
+export ANDROID_SDK_ROOT='$(realpath ./sdk)'
+" >> ~/.bashrc
+
+./$dir/bin/sdkmanager --licences
+source ~/.bashrc
+
