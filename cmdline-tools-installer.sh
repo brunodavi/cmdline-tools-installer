b19c51dfa5a3974c6201d152cd0a37fb0731a370
Bruno Davi <brunodaviandrade2000@gmail.com>
2024-07-29 22:45:38 -0300

    feat: cmdline-tools-installer.sh to many easy

--- cmdline-tools-installer.sh
+++ cmdline-tools-installer.sh
@@ -1,12 +1,29 @@
-# Dependencies
-sudo apt install openjdk-17-jdk
+sdk="\$HOME/.android-sdk"
 
-dir="sdk/cmdline-tools/latest"
+bin="$sdk/cmdline-tools/latest/bin"
 site="https://developer.android.com/studio"
 repo="https://dl.google.com/android/repository"
 
 pattern="commandlinetools-linux-\d+_latest.zip"
 name="$(curl -s $site | grep -Pom 1 $pattern)"
+
+deps_not_installed=0
+
+for cmd in java kotlin gradle
+do
+  if command -v $cmd > /dev/null; then
+    echo "WARN: $cmd not installed!"
+    deps_not_installed=1
+  fi
+done
+
+if [ deps_not_installed -eq 1 ]; then
+  echo -e '\n\nRecommend install from sdkman'
+  echo 'Site: https://sdkman.io'
+  echo 'Installation: curl -s "https://get.sdkman.io" | bash'
+  echo 'Only run: sdk install gradle kotlin java 17.0.11-tem'
+  echo 'Enjoy!'
+fi
 
 # Install command-line-tools
 wget -q --show-progress "$repo/$name" 
@@ -17,11 +34,11 @@ mv cmdline-tools/* latest/
 mv cmdline-tools/ sdk/
 mv latest sdk/cmdline-tools/
 
-echo "
-export ANDROID_SDK_ROOT='$(realpath ./sdk)'
-" >> ~/.bashrc
+echo 'Write in ~/.bash_profile or ~/.bashrc'
+echo "export ANDROID_HOME=\"$sdk\""
+
+echo 'OPTIONAL: Add Android Commands'
+echo "export PATH=\"\$PATH:$bin\""
 
 # Accept all licenses
-yes | ./$dir/bin/sdkmanager --licenses
-
-source ~/.bashrc
+yes | ./~/.android-sdk/cmdline-tools/latest/bin/sdkmanager --licenses
